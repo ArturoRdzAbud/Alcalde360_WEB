b656d647460860537553ebf3563536b16c11e9f9
ArturoRdzAbud <arturo.rodriguez.abud@gmail.com>
2024-09-03 11:51:11 -0600

    se depura el proyecto y se deja una estructura base para iniciar desarrollos.

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -11,21 +11,9 @@ import { PerfilContext } from './PerfilContext'; // Importa el contexto
 //pantallas
 import Home from './Home';
 import Login from './Login';
-import FrmLiga from './FrmLiga';
-import FrmEquipos from './FrmEquipos';
-import FrmEquipoTorneo from './FrmEquipoTorneo';
-import FrmJugador from './FrmJugador';
-import FrmEquipoJugador from './FrmEquipoJugador';
+
 import FrmArbitro from './FrmArbitro';
-import FrmTiposDeSancion from './FrmTiposDeSancion';
 import FrmUsuario from './FrmUsuario';
-import FrmConfiguraAccesoLigas from './FrmConfiguraAccesoLigas';
-
-import FrmProgramacionDePartidos from './FrmProgramacionDePartidos';
-import FrmCapturaDeResultados from './FrmCapturaDeResultados';
-
-import FrmEstadisticaPorEquipo from './FrmEstadisticaPorEquipo';
-import FrmEstadisticaJugador from './FrmEstadisticaJugador';
 
 import ProtectedRoute from './ProtectedRoute';
 import AccessDeniedPage from './AccessDeniedPage';
@@ -34,24 +22,9 @@ import AccessDeniedPage from './AccessDeniedPage';
 
 //Iconos
 import HomeSvg from '../svg/icon-home.svg?react'
-import LigaSvg from '../svg/menu-liga.svg?react'
-import EquiposSvg from '../svg/equipos.svg?react'
-import EquipoTorneoSvg from '../svg/menu-equipo-torneo.svg?react'
-import JugadoresSvg from '../svg/menu-equipo-jugador.svg?react'
-import EquipoJugadorSvg from '../svg/balon.svg?react'
 import Arbitrosvg from '../svg/arbitro.svg?react'
 import Arbitros2vg from '../svg/arbitro.svg?react'
-import Tarjetasvg from '../svg/tarjetas.svg?react'
 import UsuarioSvg from '../svg/usuario.svg?react'
-import ConfiguraAccesoSvg from '../svg/configura-acceso.svg?react'
-
-import Programacionsvg from '../svg/programacion-partidos.svg?react'
-import CapturaResultadosSvg from '../svg/menu-captura-resultados.svg?react'
-
-import ConsultaResultadosSvg from '../svg/consulta-resultados2.svg?react'
-import EstadisticaEquipoSvg from '../svg/menu-estadistica-equipo.svg?react'
-import EstadisticaJugadorSvg from '../svg/menu-estadistica-jugador.svg?react'
-import BalonSvg from '../svg/balon.svg?react'
 
 
 
@@ -74,14 +47,7 @@ export const SideBar = () => {
     };
 
 
-    const [isEstadisticasOpen, setIsEstadisticasOpen] = useState(false);
-    const toggleEstadisticas = () => {
-        setIsEstadisticasOpen(!isEstadisticasOpen);
-    };
-    const [isPartidosOpen, setIsPartidosOpen] = useState(false);
-    const togglePartidos = () => {
-        setIsPartidosOpen(!isPartidosOpen);
-    };
+    
     const [isAdminOpen, setIsAdminOpen] = useState(false);
     const toggleAdmin = () => {
         setIsAdminOpen(!isAdminOpen);
@@ -177,14 +143,7 @@ export const SideBar = () => {
                                     {isAdminOpen ? '▾' : '▸'} Configuración</div><div style={separatorStyles}></div></>)}
                                 {isAdminOpen && (
                                     <div style={fontsize}>
-                                        {perfil >= 4 && <NavLink onClick={closeMenu} to='/Liga' className='nav-link' > <LigaSvg />{' Cátalogo de Ligas '} </NavLink>}
-                                        {perfil >= 4 && <NavLink onClick={closeMenu} to='/Equipos' className='nav-link' > <EquiposSvg />{' Cátalogo de Equipos'} </NavLink>}
-                                        {perfil >= 4 && <NavLink onClick={closeMenu} to='/EquipoTorneo' className='nav-link' > <EquipoTorneoSvg />{' Configuración de Torneos'} </NavLink>}
-                                        {perfil >= 4 && <NavLink onClick={closeMenu} to='/EquipoTorneo' className='nav-link' > <EquipoTorneoSvg />{' Generar Calendario'} </NavLink>}
-                                        {perfil >= 2 && <NavLink onClick={closeMenu} to='/CatJugador' className='nav-link' > <JugadoresSvg />{' Cátalogo de Jugadores'} </NavLink>}
-                                        {perfil >= 2 && <NavLink onClick={closeMenu} to='/EquipoJugador' className='nav-link' > <EquipoJugadorSvg />{' Configuración de Plantilla de Equipos'} </NavLink>}
                                         {perfil >= 4 && <NavLink onClick={closeMenu} to='/Arbitros' className='nav-link' > <Arbitros2vg />{' Catálogo de Árbitros'} </NavLink>}
-                                        {perfil >= 4 && <NavLink onClick={closeMenu} to='/TiposDeSancion' className='nav-link' > <Tarjetasvg />{' Catálogo de Tipos de Sanción'} </NavLink>}
                                     </div>
                                 )}
                             </div>
@@ -199,32 +158,6 @@ export const SideBar = () => {
                             {perfil >= 1 &&
                                 <>
 
-                                    <div className="menu-section">
-                                        {perfil >= 3 && (<><div className="menu-section-title" onClick={togglePartidos} style={fontsizeH}>
-                                            {isPartidosOpen ? '▾' : '▸'} Administración</div><div style={separatorStyles}></div></>)}
-                                        {isPartidosOpen && (
-                                            <div style={fontsize}>
-                                                {/*Operación de las ligas y torneos*/}
-                                                {perfil >= 4 && <NavLink onClick={closeMenu} to='/ProgramacionDePartidos' className='nav-link' > <Programacionsvg />{' Programación de Partidos '} </NavLink>}
-                                                {perfil >= 3 && <NavLink onClick={closeMenu} to='/CapturaDeResultados/true' className='nav-link' > <CapturaResultadosSvg />{' Captura de Resultados '} </NavLink>}
-                                                {/*Consulta de estadisticas*/}
-                                            </div>
-                                        )}
-                                    </div>
-
-
-                                    <div className="menu-section">
-                                        <div className="menu-section-title" onClick={toggleEstadisticas} style={fontsizeH}>
-                                            {isEstadisticasOpen ? '▾' : '▸'} Analiticos</div><div style={separatorStyles}></div>
-                                        {isEstadisticasOpen && (
-                                            <div style={fontsize}>
-                                                <NavLink onClick={closeMenu} to='/CapturaDeResultados/false' className='nav-link' > <CapturaResultadosSvg />{' Consulta de Resultados '} </NavLink>
-                                                {/* <NavLink onClick={closeMenu} to='/EstEstadisticaPorEquipo' className='nav-link' > <EstadisticaEquipoSvg />{' Estadistica por Equipo '} </NavLink> */}
-                                                <NavLink onClick={closeMenu} to='/EstadisticaPorEquipo' className='nav-link' > <EstadisticaEquipoSvg />{' Estadistica por Equipo '} </NavLink>
-                                                <NavLink onClick={closeMenu} to='/EstadisticaJugador' className='nav-link' > <EstadisticaJugadorSvg />{' Estadistica por Jugador '} </NavLink>
-                                            </div>
-                                        )}
-                                    </div>
 
                                     <div className="menu-section" >
                                         <div className="menu-section-title" onClick={toggleAccesos} style={fontsizeH}>
@@ -232,7 +165,6 @@ export const SideBar = () => {
                                         {isAccesosOpen && (
                                             <div style={fontsize}>
                                                 <NavLink onClick={closeMenu} to='/FrmUsuario' className='nav-link' > <JugadoresSvg />{' Registro de Usuarios'} </NavLink>
-                                                {perfil >= 4 && <NavLink onClick={closeMenu} to='/FrmConfiguraAccesoLigas' className='nav-link' > <ConfiguraAccesoSvg />{' Configura Acceso a Ligas y Torneos'} </NavLink>}
                                             </div>
                                         )}
                                     </div>
@@ -252,28 +184,11 @@ export const SideBar = () => {
                         <Route element={<ProtectedRoute profile={perfil} requiredProfile={1} />}><Route path='/Login' element={<Login />} /></Route>
                         <Route element={<ProtectedRoute profile={perfil} requiredProfile={1} />}><Route path="/access-denied" element={<AccessDeniedPage />} /></Route>
 
-                        {/*Configuración inicial*/}
-                        <Route element={<ProtectedRoute profile={perfil} requiredProfile={4} />}><Route path='/Liga' element={<FrmLiga />} /></Route>
-                        <Route element={<ProtectedRoute profile={perfil} requiredProfile={4} />}><Route path='/Equipos' element={<FrmEquipos />} /></Route>
-                        <Route element={<ProtectedRoute profile={perfil} requiredProfile={4} />}><Route path='/EquipoTorneo' element={<FrmEquipoTorneo />} /></Route>
-                        <Route element={<ProtectedRoute profile={perfil} requiredProfile={2} />}><Route path='/Jugador' element={<FrmJugador />} /></Route>
-                        <Route element={<ProtectedRoute profile={perfil} requiredProfile={2} />}><Route path='/EquipoJugador' element={<FrmEquipoJugador />} /></Route>
                         <Route element={<ProtectedRoute profile={perfil} requiredProfile={4} />}><Route path='/Arbitros' element={<FrmArbitro />} /></Route>
-                        <Route element={<ProtectedRoute profile={perfil} requiredProfile={4} />}><Route path='/TiposDeSancion' element={<FrmTiposDeSancion />} /></Route>
-
-                        {/*Operación de las ligas y torneos*/}
-                        <Route element={<ProtectedRoute profile={perfil} requiredProfile={4} />}><Route path='/ProgramacionDePartidos' element={<FrmProgramacionDePartidos />} /></Route>
-                        <Route element={<ProtectedRoute profile={perfil} requiredProfile={1} />}><Route path='/CapturaDeResultados/:muestraLinkCaptura' element={<FrmCapturaDeResultados />} /></Route>
-
-                        {/*Consulta de estadisticas*/}
-                        <Route element={<ProtectedRoute profile={perfil} requiredProfile={1} />}><Route path='/CapturaDeResultados/:muestraLinkCaptura' element={<FrmCapturaDeResultados />} /></Route>
-                        <Route element={<ProtectedRoute profile={perfil} requiredProfile={1} />}><Route path='/EstadisticaPorEquipo' element={<FrmEstadisticaPorEquipo />} /></Route>
-                        <Route element={<ProtectedRoute profile={perfil} requiredProfile={1} />}><Route path='/EstadisticaJugador' element={<FrmEstadisticaJugador />} /></Route>
+                       
 
                         <Route element={<ProtectedRoute profile={perfil} requiredProfile={1} />}><Route path='/FrmUsuario' element={<FrmUsuario />} /></Route>
-                        {/* <Route path='/FrmConfiguraAccesoLigas' element={<FrmConfiguraAccesoLigas />} /> */}
-                        <Route element={<ProtectedRoute profile={perfil} requiredProfile={4} />}><Route path='/FrmConfiguraAccesoLigas' element={<FrmConfiguraAccesoLigas />} /></Route>
-
+                        
                         {/* <ProtectedRoute path="/Liga" element={<CatLiga />} profile={perfil} requiredProfile={2}/> */}
                         {/* <Route path="/Liga" element={<ProtectedRoute profile={perfil} requiredProfile={2} />}/> */}
 
