14757854031b894bd4fade4efd7f5b0acc250f91
danielmendez805 <dan805@gmail.com>
2024-09-24 13:46:05 -0600

    Avance consulta incidencia

--- src/components/PerfilContext.jsx
+++ src/components/PerfilContext.jsx
@@ -9,17 +9,17 @@ export const PerfilProvider = ({ children }) => {
   const [perfil, setPerfil] = useState(4);//devteam temporalmente default seras admin
   const [esConLicencia, setEsConLicencia] = useState(0);
   const [nombreUsuario, setNombreUsuario] = useState('');
-  const [idAlcaldia, setIdAlcaldia] = useState(0);
+  const [idAlcaldia, setIdAlcaldia] = useState(1);
   const [nomAlcaldia, setNomAlcaldia] = useState('');
 
   return (
-    <PerfilContext.Provider value={{ 
-             perfil, setPerfil
-            ,esConLicencia, setEsConLicencia
-            ,nombreUsuario,setNombreUsuario
-            ,idAlcaldia,setIdAlcaldia
-            ,nomAlcaldia,setNomAlcaldia
-            }}>
+    <PerfilContext.Provider value={{
+      perfil, setPerfil
+      , esConLicencia, setEsConLicencia
+      , nombreUsuario, setNombreUsuario
+      , idAlcaldia, setIdAlcaldia
+      , nomAlcaldia, setNomAlcaldia
+    }}>
       {children}
     </PerfilContext.Provider>
   );
